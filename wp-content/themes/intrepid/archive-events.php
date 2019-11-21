8a03e2fa0b18659a5423450e58eaf8350e30b990
jrep20 <jrep20@gmail.com>
2019-11-21 15:31:40 -0500

    adjust news posts

--- wp-content/themes/intrepid/archive-events.php
+++ wp-content/themes/intrepid/archive-events.php
@@ -3,7 +3,7 @@ get_header();
 the_post();
 ?>
 
-<main class="main">
+<main class="main page-events-archive">
     <section class="hero hero--inner" style="background-image:url(<?php echo STYLEDIR; ?>/uploads/our-models-bg-image.jpg);">
         <div class="container">
             <div class="hero__content">
@@ -17,30 +17,38 @@ the_post();
             <div class="column-block__wrap">
                 <div class="column-block__item">
                     <figure class="column-block__thumbnail">
-                        <img src="<?php echo STYLEDIR; ?>/uploads/beach.jpg" alt="Beach"/>
+                        <a href="<?php echo site_url('events/sample-event'); ?>">
+                            <img src="<?php echo STYLEDIR; ?>/uploads/beach.jpg" alt="Beach"/>
+                        </a>
                     </figure>
-                    <h2 class="column-block__title">Intrepid Owner's Rendevous</h2>
+                    <h2 class="column-block__title">
+                        <a href="<?php echo site_url('events/sample-event'); ?>">Intrepid Owner's Rendevous</a>
+                    </h2>
                     <span class="column-block__date">July 12 - 14, 2019</span>
                     <div class="column-block__content">
                         <p>Join us for the first ever Intrepid Owners Rendezvous at Hawks Cay Resort July 12th-14th. It’s going to be a full weekend of fun, relaxation and entertainment exclusively for Intrepid owners. Reserve your spot now for this inaugural event before May 17th. We hope to see you there for an unforgettable weekend.</p>
                     </div>
                     <div class="btn-group">
                         <a href="#" class="btn btn--outline">Reserve a Spot</a>
-                        <a href="#" class="btn btn--outline">More Info</a>
+                        <a href="<?php echo site_url('events/sample-event'); ?>" class="btn btn--outline">More Info</a>
                     </div>
                 </div>
                 <div class="column-block__item">
                     <figure class="column-block__thumbnail">
-                        <img src="<?php echo STYLEDIR; ?>/uploads/beach.jpg" alt="Beach"/>
+                        <a href="<?php echo site_url('events/sample-event'); ?>">
+                            <img src="<?php echo STYLEDIR; ?>/uploads/beach.jpg" alt="Beach"/>
+                        </a>
                     </figure>
-                    <h2 class="column-block__title">Intrepid Owner's Rendevous</h2>
+                    <h2 class="column-block__title">
+                        <a href="<?php echo site_url('events/sample-event'); ?>">Intrepid Owner's Rendevous</a>
+                    </h2>
                     <span class="column-block__date">July 12 - 14, 2019</span>
                     <div class="column-block__content">
                         <p>Fort Lauderdale, Florida, the "Yachting Capital of the World" will host the 60th annual Fort Lauderdale International Boat Show on October 30 - Nov 3, 2019.</p>
                         <p>Every year, the show exhibits a vast array of the industry's latest boats and yachts of all sizes, worldwide debuts, plus a medley of marine products and accessories to enhance the nautical lifestyle. From yacht builders and designers to exotic cars and brokerage yachts, this show has something for everyone!</p>
                     </div>
                     <div class="btn-group">
-                        <a href="#" class="btn btn--outline">More Info</a>
+                        <a href="<?php echo site_url('events/sample-event'); ?>" class="btn btn--outline">More Info</a>
                     </div>
                 </div>
             </div>
--- wp-content/themes/intrepid/single-events.php
+++ wp-content/themes/intrepid/single-events.php
@@ -2,7 +2,7 @@
 get_header();
 the_post();
 ?>
-<main class="main">
+<main class="main page-events-single">
     <section class="hero hero--inner hero--news-single" style="background-image:url(<?php echo STYLEDIR; ?>/uploads/news_hero.jpg);">
         <img class="hero__mobile-image" src="<?php echo STYLEDIR; ?>/uploads/news_hero_mobile.jpg" alt="news_hero" />
         <a href="#" class="arrow-link"><svg class="icon icon-arrow-up" aria-hidden="true" role="img"><use xlink:href="#icon-arrow-up" x="0" y="0"></use></svg>Back to main page</a>
@@ -11,25 +11,25 @@ the_post();
         <div class="container">
             <div class="sidebar layout__one-third">
                 <div class="article-block">
-                    <h2 class="section-title section-title--gray">Related Articles</h2>
+                    <h2 class="section-title section-title--gray">More Events</h2>
                     <ul class="article-list">
                         <li class="article-list__item">
-                            <h3 class="article-list__title"><a href="#">March 2019 Newsletter</a></h3>
+                            <h3 class="article-list__title"><a href="#">Event Title</a></h3>
                             <p class="article-list__short-desc">Let me start this month’s newsletter by inviting everyone to the Palm Beach Boat Show which runs from Thursday, March 28th through Sun ...</p>
                             <a href="#" class="link">Read More</a>
                         </li>
                         <li class="article-list__item">
-                            <h3 class="article-list__title"><a href="#">November 2017 Newsletter</a></h3>
+                            <h3 class="article-list__title"><a href="#">Event Title</a></h3>
                             <p class="article-list__short-desc">We scheduled this newsletter to go out Thanksgiving morning because it is a great time for us to give thanks to all towards the new boat ...</p>
                             <a href="#" class="link">Read More</a>
                         </li>
                         <li class="article-list__item">
-                            <h3 class="article-list__title"><a href="#">Invitation to the pre miami international boat show</a></h3>
+                            <h3 class="article-list__title"><a href="#">Event Title</a></h3>
                             <p class="article-list__short-desc">On February 1st, 2018 Intrepid would like to invite you to our Pre Miami International Boat Show Event located ...</p>
                             <a href="#" class="link">Read More</a>
                         </li>
                         <li class="article-list__item">
-                            <h3 class="article-list__title"><a href="#">410 Evolution Updates</a></h3>
+                            <h3 class="article-list__title"><a href="#">Event Title</a></h3>
                             <p class="article-list__short-desc">Let me start this month’s newsletter by inviting everyone to the Palm Beach Boat Show which runs from Thursday, March 28th through Sun ...</p>
                             <a href="#" class="link">Read More</a>
                         </li>
@@ -37,7 +37,7 @@ the_post();
                 </div>
             </div>
             <div class="layout__two-third">
-                <h1 class="page-title">April 2019 Newsletter</h1>
+                <h1 class="page-title">Sample Event</h1>
                 <div class="newsletter-article">
                     <article class="newsletter-article__item entry-content">
                         <div class="post-author">
@@ -52,7 +52,8 @@ the_post();
                             April 20, 2019
                         </span>
                         <div class="content-block">
-                            <p>I want to start this month’s newsletter off with a big thank you to everyone that came out to the Palm Beach Boat Show last month. It was the best Palm Beach Show that we have ever had. It actually exceeded the Miami Show for us. If you would have told me that a few years ago that the Palm Beach Show would top the Miami Show for new sales I would have told you that you were crazy. This show really has become quite the venue.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut risus vitae tellus efficitur condimentum sed et ipsum. Proin condimentum iaculis euismod. Nulla facilisi. Aenean risus dolor, maximus in sollicitudin eu, dictum sed erat. Ut scelerisque blandit est, sed aliquam nisi viverra nec. Fusce gravida tempus orci, eget lacinia urna commodo ac. Curabitur et magna eros. Integer sagittis sagittis metus non tristique. Vivamus sit amet venenatis eros. Aliquam dolor nisl, vestibulum sed metus et, luctus pulvinar nisl. Curabitur dignissim hendrerit lacus, id vehicula lectus. Donec condimentum commodo magna, elementum imperdiet neque varius at. Quisque dignissim placerat posuere. Pellentesque eu erat imperdiet, dignissim magna vitae, gravida tellus. Praesent eu orci nec tortor rutrum venenatis.</p>
+                            <p>Proin volutpat justo at mauris vulputate, at eleifend mauris efficitur. Nulla ex risus, bibendum ut mi vel, tempus aliquam elit. Nam in libero purus. Vivamus et consectetur nisi, in varius metus. In mollis velit a diam pulvinar ullamcorper ut et lorem. Integer ut felis non ipsum pretium faucibus. Nunc lacinia quis erat in fermentum. Praesent molestie dui justo, ut pharetra sapien lacinia quis. Vestibulum in venenatis sem. Fusce sit amet leo id risus aliquet consectetur. Nullam lacinia porttitor orci eu feugiat. In iaculis velit sit amet odio imperdiet ultricies. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec non odio nec arcu semper gravida. Aliquam sit amet ante neque.</p>
                         </div>
                         <div class="slider-wrap">
                             <div class="image-slider">
@@ -69,73 +70,7 @@ the_post();
                             <span class="slider-caption">Photos from the palm beach show</span>
                         </div>
                         <div class="content-block">
-                            <p>It probably did not hurt that we also debuted a boat there for the first time. The new 375 SE Nomad was a big hit. The entire Nomad series has really taken off and we are already prototyping the next in this series, the 407 FE and SE Nomad.</p>
-                        </div>
-                        <div class="slider-wrap">
-                            <div class="image-slider">
-                                <div>
-                                    <img src="<?php echo STYLEDIR; ?>/uploads/slider-image2.jpg" alt="slider-image2" />
-                                </div>
-                                <div>
-                                    <img src="<?php echo STYLEDIR; ?>/uploads/slider-image3.jpg" alt="slider-image3" />
-                                </div>
-                                <div>
-                                    <img src="<?php echo STYLEDIR; ?>/uploads/slider-image1.jpg" alt="slider-image1" />
-                                </div>
-                            </div>
-                            <span class="slider-caption">The 407 fe and se nomad</span>
-                        </div>
-                        <div class="content-block">
-                            <p>The other new boat that we are diligently working on is the new 345 Walkaround. We are creating the deck plug now and we have the mockups of the cabin that we are working on. It is one thing to draw it but we really need to go full mock up on new models so you can literally feel the space and the actual usability. More times than not what we draw and what we actually end up with are very different after the mockup stage. Here are some shots we took during mock up on the 345 Walkaround. We have already taken deposits on all three of these new models so if you want more information on any of these and want to get on the waiting list, <a href="#">please reach out to our Dania Sales office.</a></p>
-                        </div>
-                        <div class="slider-wrap">
-                            <div class="image-slider">
-                                <div>
-                                    <img src="<?php echo STYLEDIR; ?>/uploads/slider-image3.jpg" alt="slider-image3" />
-                                </div>
-                                <div>
-                                    <img src="<?php echo STYLEDIR; ?>/uploads/slider-image1.jpg" alt="slider-image1" />
-                                </div>
-                                <div>
-                                    <img src="<?php echo STYLEDIR; ?>/uploads/slider-image2.jpg" alt="slider-image2" />
-                                </div>
-                            </div>
-                            <span class="slider-caption">345 Walkaround progress photos</span>
-                        </div>
-                        <div class="content-block">
-                            <p>Another new project that we have started working on is our new video owner’s manuals. Our VP of Customer Service Joe Brenna has taken on this project and has one model finished and under his belt and that is the 407 Panacea. We have this on our YouTube channel now and we will be adding this tab to our website and our owner’s portal. You will see that it is indexed by subject matter so that you can go right to the section that you are most interested in. We plan on doing this for every model and also creating an app to access them as well. So, a big shout out to Joe and his partner in this project our VP of Engineering Richard Ahl for the extra time that these guys have put in and will continue to put in in order to get all of this information out to you for you to use. Good stuff!</p>
-                            <p><a target="_blank" href="https://www.youtube.com/watch?v=Imv0CLWvjzs&list=PL7iop12r98ZIK_wlXMruUVWXqhG8rv_n5&index=2&t=0s">https://www.youtube.com/watch?v=Imv0CLWvjzs&list=PL7iop12r98ZIK_wlXMruUVWXqhG8rv_n5&index=2&t=0s</a></p>
-                            <p>Besides the Nomad Series boats the <a href="#">407 Cuddy</a> and <a href="#">Panacea</a> continue to sell well along with the <a href="#">327 Open</a>. The other model that seems to be on fire right now is the <a href="#">475 Sport Yacht</a>. The Sport Yacht line that we build these on usually splits build slots with <a href="#">475 Panaceas</a>, <a href="#">410 Evolutions</a>, and <a href="#">430 Sport yachts</a> but right now I have (5) <a href="#">475 Sport Yachts</a> lined up in a row! It is definitely the Sport Yacht of choice for this first part of 2019!!</p>
-                            <p>The next show that we are attending is the Suncoast Boat Showstarting this Friday 4/26 and running through Sunday the 28th in Sarasota. We are bringing our latest model the <a href="#">375 SE Nomad</a>, a <a href="#">327 Center Console</a>, and the mighty <a href="#">200 flats boat</a>! Our own Mark Beaver and Alex Rizo will be there to answer everyone’s questions and show you our latest and greatest. We hope to see everyone down there!</p>
-                        </div>
-                        <div class="signature-block">
-                            <img src="<?php echo STYLEDIR; ?>/uploads/signature.png" alt="signature" />
-                        </div>
-                    </article>
-                    <article class="newsletter-article__item entry-content">
-                        <div class="post-author">
-                            <figure class="post-author__photo">
-                                <img src="<?php echo STYLEDIR; ?>/uploads/author1.jpg" alt="author1" />
-                            </figure>
-                            <span class="post-author__name">
-                                Alex Rizo, Vice President of Sales
-                            </span>
-                        </div>
-                        <div class="content-block entry-content">
-                            <p>Very happy to report that the Palm Beach Boat Show was a huge success and I would like to personally thank everyone that came by to see us. Besides having the time to visit with our existing customers, we also made many new friends with a great number of new boat orders. We’re already looking forward to being there again next year.</p>
-                            <p>This was the first showing of the new 375 Nomad. It received amazing reviews from both our customers and the media that came to interview us and write several upcoming articles.</p>
-                            <p>This month we're headed to the Suncoast Boat Show in Sarasota, Florida, April 26, 27, & 28 where we will have the 375 Nomad, 327I Center Console and our 200 Flats Boat on display. Mark Beaver, Barrett Grene and I will all be attending. We are very excited to be going to a West Coast of Florida boat show and seeing many of our friends and customers in this small venue setting which gives us even more time to spend together. With summer around the corner and the boat show season coming to an end please make your plans to visit with us.</p>
-                            <span>Suncoat Boat show line up:</span><br>
-                            <ul>
-                                <li><a href="#">375 Nomad</a></li>
-                                <li><a href="#">327I Center Console</a></li>
-                                <li><a href="#">200 Flats Boat</a></li>
-                            </ul>
-                            <p>Below is a link for more information on the Suncoast Boat Show: <br>https://www.suncoastboatshow.com/en/home.html</p>
-                            <p>We all look forward to seeing you there. </p>
-                        </div>
-                        <div class="signature-block">
-                            <img src="<?php echo STYLEDIR; ?>/uploads/signature.png" alt="signature" />
+                            <p>Proin volutpat justo at mauris vulputate, at eleifend mauris efficitur. Nulla ex risus, bibendum ut mi vel, tempus aliquam elit. Nam in libero purus. Vivamus et consectetur nisi, in varius metus. In mollis velit a diam pulvinar ullamcorper ut et lorem. Integer ut felis non ipsum pretium faucibus. Nunc lacinia quis erat in fermentum. Praesent molestie dui justo, ut pharetra sapien lacinia quis. Vestibulum in venenatis sem. Fusce sit amet leo id risus aliquet consectetur. Nullam lacinia porttitor orci eu feugiat. In iaculis velit sit amet odio imperdiet ultricies. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec non odio nec arcu semper gravida. Aliquam sit amet ante neque.</p>
                         </div>
                     </article>
                 </div>
