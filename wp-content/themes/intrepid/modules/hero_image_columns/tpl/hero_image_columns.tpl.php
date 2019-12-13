93c8472b8c3ad93f6fcd9922970402794654e377
bob <bob@e11group.com>
2019-12-13 12:21:03 -0500

    hero image column module

---
+++ wp-content/themes/intrepid/modules/hero_image_columns/tpl/hero_image_columns.tpl.php
@@ -0,0 +1,61 @@
+<section class="landing-hero landing-hero--alt">
+    <div class="container">
+        <?php if($data['column_1']) : ?>
+            <div class="landing-hero__item landing-hero__item--active">
+                <div class="landing-hero__item-bg" style="background-image:url('<?php echo $data['column_1']['image']['url']; ?>')"></div>
+
+                <div class="landing-hero__content landing-hero__content--fade">
+                    <?php if($data['column_1']['title']) : ?>
+                        <h2 class="landing-hero__title"><?php echo $data['column_1']['title']; ?></h2>
+                    <?php endif; ?>
+
+                    <?php if($data['column_1']['content']) : ?>
+                        <div class="landing-hero__description">
+                            <?php echo $data['column_1']['content']; ?>
+                        </div>
+                    <?php endif; ?>
+
+                    <?php if($data['column_1']['link']) : ?>
+                        <a class="landing-hero__link" href="<?php echo $data['column_1']['link']['url']; ?>" target="?php echo $data['column_1']['link']['target']; ?>">
+                            <span class="btn btn--outline btn--light">"<?php echo $data['column_1']['link']['title']; ?></span>
+                        </a>
+                    <?php endif; ?>
+                </div>
+            </div>
+        <?php endif; ?>
+
+        <?php if($data['column_2']) : ?>
+            <div class="landing-hero__item">
+                <div class="landing-hero__item-bg" style="background-image:url('<?php echo $data['column_2']['image']['url']; ?>')"></div>
+                <div class="landing-hero__content">
+                    <?php if ($data['column_2']['title']) : ?>
+                        <h2 class="landing-hero__title"><?php echo $data['column_2']['title']; ?></h2>
+                    <?php endif; ?>
+
+                    <?php if ($data['column_2']['content']) : ?>
+                        <div class="landing-hero__description">
+                        <?php echo $data['column_2']['content']; ?>
+                    </div>
+                    <?php endif; ?>
+
+                    <?php if ($data['column_2']['link']) : ?>
+                        <a class="landing-hero__link" href="<?php echo $data['column_2']['link']['url']; ?>" target="<?php echo $data['column_2']['link']['target']; ?>">
+                            <span class="btn btn--outline btn--light"><?php echo $data['column_2']['link']['title']; ?></span>
+                        </a>
+                    <?php endif; ?>
+
+
+                </div>
+            </div>
+        <?php endif; ?>
+
+        <?php if($data['intro_text']): ?>
+            <div class="landing-hero__intro">
+                <?php echo $data['intro_text']; ?>
+            </div>
+        <?php endif; ?>
+
+    </div>
+</section>
+
+
--- wp-content/themes/intrepid/template-about.php
+++ wp-content/themes/intrepid/template-about.php
@@ -7,36 +7,8 @@ the_post();
 ?>
 <?php get_header(); ?>
 <main class="main">
-    <section class="landing-hero landing-hero--alt">
-        <div class="container">
-            <div class="landing-hero__item landing-hero__item--active">
-                <div class="landing-hero__item-bg" style="background-image:url(<?php echo STYLEDIR; ?>/uploads/fishing.jpg)"></div>
-                <div class="landing-hero__content landing-hero__content--fade">
-                    <h2 class="landing-hero__title">One of a kind</h2>
-                    <div class="landing-hero__description">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed post.</p>
-                    </div>
-                    <a class="landing-hero__link" href="<?php echo site_url('about/one-of-a-kind'); ?>">
-                        <span class="btn btn--outline btn--light">Custom Made</span>
-                    </a>
-                </div>
-            </div>
-            <div class="landing-hero__item">
-                <div class="landing-hero__item-bg" style="background-image:url(<?php echo STYLEDIR; ?>/uploads/craftsmanship.jpg)"></div>
-                <div class="landing-hero__content">
-                    <h2 class="landing-hero__title">One at a time</h2>
-                    <div class="landing-hero__description">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed post.</p>
-                    </div>
-                    <a class="landing-hero__link" href="<?php echo site_url('about/one-at-a-time'); ?>">
-                        <span class="btn btn--outline btn--light">Expert Craftmanship</span>
-                    </a>
-                </div>
-            </div>
-            <div class="landing-hero__intro">
-                <p>From the moment you decide to own an Intrepid, you will enter a world of collaborative design, precision construction and customization and one of the most coveted and legendary customer service programs on land or sea. </p>
-            </div>
-        </div>
-    </section>
+
+    <?php e11_hero_image_columns(array(), true, 'get_field', $page, ''); ?>
+
 </main>
 <?php get_footer(); ?>
