d7d463941b3927e6f2cf440959dc294d014b11c5
bob <bob@e11group.com>
2019-12-13 10:26:09 -0500

    hero video module edits

--- wp-content/themes/intrepid/modules/hero_video/tpl/hero_video.tpl.php
+++ wp-content/themes/intrepid/modules/hero_video/tpl/hero_video.tpl.php
@@ -3,12 +3,11 @@
         <?php if ($data['video'] || $data['title']) : ?>
             <div class="hero__content">
                 <?php if ($data['title']) : ?>
-                    <!-- <h1 class="hero__title"><strong>The Intrepid</strong> Difference</h1> -->
                     <h1 class="hero__title"><?php echo $data['title']; ?></h1>
 
                 <?php endif; ?>
                 <?php if ($data['video']) : ?>
-                    <a data-fancybox href="<?php echo $data['video']; ?>" class="play-btn">Play Video</a>
+                    <a href="<?php echo $data['video']; ?>" data-fancybox  class="play-btn">Play Video</a>
                 <?php endif; ?>
             </div>
         <?php endif; ?>
