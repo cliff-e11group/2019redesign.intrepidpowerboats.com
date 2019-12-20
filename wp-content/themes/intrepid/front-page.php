740f832aa184cc56e4d3496c44b0804de481d92e
bob <bob@e11group.com>
2019-12-20 14:42:47 -0500

    front page newsletter form

--- wp-content/themes/intrepid/front-page.php
+++ wp-content/themes/intrepid/front-page.php
@@ -22,39 +22,18 @@ $page = get_the_ID();
         </h3>
         <div class="newsletter-block__content">
             <div class="newsletter-block__description">
-                <p>Stay up to date with all the latest Intrepid information! In our newsletter you'll hear from the President of Intrepid, Ken Clinton, as well as other team members.</p>
+                <?php
+                    $newsletter_description = get_field('newsletter_description', 'option');
+                ?>
+                <?php if ($newsletter_description) : ?>
+                    <p><?php echo $newsletter_description; ?></p>
+                <?php endif; ?>
+
                 <span class="newsletter-block__info">Enter Information Below:</span>
             </div>
-            <div class="gf_browser_chrome gform_wrapper">
-                <a id="gf_3" class="gform_anchor"></a>
-                <form method="post" enctype="multipart/form-data" target="gform_ajax_frame_3" id="gform_3" action="#">
-                    <div class="gform_body">
-                        <ul class="gform_fields top_label form_sublabel_below description_below">
-                            <li class="gfield field__firstname field_sublabel_below field_description_below gfield_visibility_visible">
-                                <label class="gfield_label" for="input_3_8">First Name</label>
-                                <div class="ginput_container ginput_container_text">
-                                    <input name="input_8" id="input_3_8" type="text" value="" class="medium" tabindex="5" placeholder="First Name" aria-invalid="false">
-                                </div>
-                            </li>
-                            <li class="gfield field__lastname field_sublabel_below field_description_below gfield_visibility_visible">
-                                <label class="gfield_label" for="input_3_9">Last Name</label>
-                                <div class="ginput_container ginput_container_text">
-                                    <input name="input_9" id="input_3_9" type="text" value="" class="medium" tabindex="6" placeholder="Last Name" aria-invalid="false">
-                                </div>
-                            </li>
-                            <li class="gfield field__email gfield_contains_required field_sublabel_below field_description_below gfield_visibility_visible">
-                                <label class="gfield_label" for="input_3_5">Work Email<span class="gfield_required">*</span></label>
-                                <div class="ginput_container ginput_container_email">
-                                    <input name="input_5" id="input_3_5" type="email" value="" class="medium" tabindex="9" placeholder="Work Email" aria-required="true" aria-invalid="false">
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="gform_footer top_label">
-                        <input type="submit" class="gform_button button" value="Submit" tabindex="11">
-                    </div>
-                </form>
-            </div>
+
+            <?php echo do_shortcode('[gravityform id="1" title="false" description="false" ajax="true"]'); ?>
+
         </div>
     </div>
     <div class="language-switcher">
