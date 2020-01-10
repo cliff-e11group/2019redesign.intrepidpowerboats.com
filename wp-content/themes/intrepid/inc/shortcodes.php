0e47cbea3dfbc1e82d8e2e558e309efa7a194c2d
bob <bob@e11group.com>
2020-01-10 15:04:47 -0500

    add shortcode to wysiwig editor tool bar

--- wp-content/themes/intrepid/inc/shortcodes.php
+++ wp-content/themes/intrepid/inc/shortcodes.php
@@ -4,3 +4,31 @@ function e11_button_shortcode($atts = [], $content = null) {
 	return '<button class="btn btn--dark">'.$content.'</button>';
 }
 add_shortcode('button', 'e11_button_shortcode');
+
+add_action('admin_init', 'e11_button_shortcode_select');
+ function e11_button_shortcode_select() {
+
+      //Abort early if the user will never see TinyMCE
+      if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') && get_user_option('rich_editing') == 'true')
+           return;
+
+      //Add a callback to regiser our tinymce plugin
+      add_filter('mce_external_plugins', 'e11_button_register_tinymce_plugin');
+
+      // Add a callback to add our button to the TinyMCE toolbar
+      add_filter('mce_buttons', 'e11_add_tinymce_button');
+}
+
+//This callback registers our plug-in
+function e11_button_register_tinymce_plugin($plugin_array) {
+    $plugin_array['e11_button'] = get_stylesheet_directory_uri() . '/js/button_shortcode.js';
+    return $plugin_array;
+}
+
+//This callback adds our button to the toolbar
+function e11_add_tinymce_button($buttons) {
+            //Add the button ID to the $button array
+    $buttons[] = "e11_button";
+    // echo '<pre>'; print_r($buttons);
+    return $buttons;
+}
---
+++ wp-content/themes/intrepid/js/button_shortcode.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function($) {
+
+    tinymce.create('tinymce.plugins.e11_button_plugin', {
+        init : function(ed, url) {
+                // Register command for when button is clicked
+                ed.addCommand('e11_insert_shortcode', function() {
+                    selected = tinyMCE.activeEditor.selection.getContent();
+
+                    if( selected ){
+                        //If text is selected when button is clicked
+                        //Wrap shortcode around it.
+                        content =  '[button class="btn btn--dark"]'+selected+'[/button]';
+                    }else{
+                        content =  '[button][/button]';
+                    }
+
+                    tinymce.execCommand('mceInsertContent', false, content);
+                });
+
+            // Register buttons - trigger above command when clicked
+            ed.addButton('e11_button', {title : 'Insert button', cmd : 'e11_insert_shortcode', text: 'AddBbutton' });
+        },
+    });
+
+    // Register our TinyMCE plugin
+    // first parameter is the button ID1
+    // second parameter must match the first parameter of the tinymce.create() function above
+    tinymce.PluginManager.add('e11_button', tinymce.plugins.e11_button_plugin);
+});
