8a70fc58598d6e936e2166f05581ea4f440690bd
jrep20 <jrep20@gmail.com>
2020-01-15 11:58:53 -0500

    fixed shortcode button styles

--- wp-content/themes/intrepid/inc/shortcodes.php
+++ wp-content/themes/intrepid/inc/shortcodes.php
@@ -1,7 +1,7 @@
 <?php
 
 function e11_button_shortcode($atts = [], $content = null) {
-	return '<button class="btn btn--outline-dark btn--outline">'.$content.'</button>';
+	return '<div class="btn-shortcode">'.$content.'</div>';
 }
 add_shortcode('button', 'e11_button_shortcode');
 
