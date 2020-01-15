7456250e6e756d7d625c1d6f347d1e6c5e545890
jrep20 <jrep20@gmail.com>
2020-01-15 10:01:33 -0500

    fixed button shortcode classes

--- wp-content/themes/intrepid/inc/shortcodes.php
+++ wp-content/themes/intrepid/inc/shortcodes.php
@@ -1,7 +1,7 @@
 <?php
 
 function e11_button_shortcode($atts = [], $content = null) {
-	return '<button class="btn btn--dark">'.$content.'</button>';
+	return '<button class="btn btn--outline-dark btn--outline">'.$content.'</button>';
 }
 add_shortcode('button', 'e11_button_shortcode');
 
