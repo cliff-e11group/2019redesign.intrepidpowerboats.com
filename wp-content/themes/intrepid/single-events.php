f948d10cae05b3a1c0c319b1e89ea4d53d0018c2
bob <bob@e11group.com>
2020-01-16 13:54:59 -0500

    Remove Header and image inside of the "more info" page

--- wp-content/themes/intrepid/single-events.php
+++ wp-content/themes/intrepid/single-events.php
@@ -5,24 +5,7 @@ $page = get_the_ID();
 
 ?>
 <main class="main page-events-single">
-    <section class="hero hero--inner" style="background-image:url(<?php echo STYLEDIR; ?>/uploads/our-models-bg-image.jpg);">
-        <div class="container">
-            <div class="hero__content">
-                <h1 class="hero__title"><?php echo get_the_title(); ?></h1>
-                <!-- <div class="hero__description">
-                    <p><?php //echo $tagline; ?></p>
-                </div> -->
-            </div>
-        </div>
-    </section>
-    <div class="model-intro">
-        <div class="model-intro__thumbnail" style="background-image:url(<?php echo get_the_post_thumbnail_url($page, 'hero'); ?>)"></div>
-        <div class="container">
-            <div class="model-intro__content">
-                <p><?php echo get_field('event_intro'); ?></p>
-            </div>
-        </div>
-    </div>
+
 
     <?php
     $content_blocks = get_field('content_blocks');
