9273cd8410fac82e373f3135365bbef7e759e97d
jrep20 <jrep20@gmail.com>
2020-02-04 12:23:31 -0500

    added FB pixel snippet

--- wp-content/themes/intrepid/header.php
+++ wp-content/themes/intrepid/header.php
@@ -21,6 +21,24 @@
 <!-- End Google Tag Manager (noscript) -->
 <div id="page" class="site">
     <header class="header">
+        <!-- Facebook Pixel Code -->
+        <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window,document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '229279001215207');
+            fbq('track', 'PageView');
+        </script>
+        <noscript>
+            <img height="1" width="1" src="https://www.facebook.com/tr?id=229279001215207&ev=PageView&noscript=1"/>
+        </noscript>
+        <!-- End Facebook Pixel Code -->
         <div class="header__wrap">
             <div class="header__branding">
                 <a href="<?php echo site_url(); ?>" class="logo"><img src="<?php echo IMAGES; ?>/logo.svg" alt="Intrepid Powerboats"/></a>
