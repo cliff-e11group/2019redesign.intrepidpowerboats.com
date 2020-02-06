d7ce4843e136eeb921a5f79483b1e1a826b304dc
bob <bob@e11group.com>
2020-02-06 16:30:54 -0500

    fix images not showing in image galleries

--- wp-content/themes/intrepid/modules/image_gallery/tpl/image_gallery.tpl.php
+++ wp-content/themes/intrepid/modules/image_gallery/tpl/image_gallery.tpl.php
@@ -1,7 +1,7 @@
 <div class="slider-wrap">
     <div class="image-slider">
         <?php foreach($data['gallery'] as $image) : ?>
-            <img src="<?php echo $image['sizes']['image-gallery']; ?>" alt="<?php echo $image['alt']; ?>">
+            <img src="<?php echo $image['image']['sizes']['image-gallery']; ?>" alt="<?php echo $image['alt']; ?>">
         <?php endforeach; ?>
     </div>
 </div>
