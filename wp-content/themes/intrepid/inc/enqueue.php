bf84a5ff394f716d6857a61eb54cd462cf6508e3
bob <bob@e11group.com>
2020-02-13 13:09:22 -0500

    add eye svg to bab

--- wp-content/themes/intrepid/inc/enqueue.php
+++ wp-content/themes/intrepid/inc/enqueue.php
@@ -15,10 +15,10 @@ function e11_scripts(){
 
 
 		// Main Stylsheet
-		wp_enqueue_style('css', STYLEDIR.'/style.css', false, '1.0.3');
+		wp_enqueue_style('css', STYLEDIR.'/style.css', false, '1.0.4');
 
 		// Main Scripts (this file is concatenated from the files inside of js/development/ )
-		wp_enqueue_script('scripts', JSDIR.'/scripts.min.js', array('jquery', 'object-assign-polyfill'), '1.0.3', true);
+		wp_enqueue_script('scripts', JSDIR.'/scripts.min.js', array('jquery', 'object-assign-polyfill'), '1.0.4', true);
         wp_localize_script(
 			'scripts',
 			'localized',
